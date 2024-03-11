50415918e989c62c3776b47b978a8a32e7609395
Vismaya Ajikumar <92380865+vismayaajikumar@users.noreply.github.com>
2024-03-11 22:45:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, world! This is PES2UG21CS613 and PES2UG21CS569" << std::endl;
+    std::cout << "Hello, world! This is PES2UG21CS63 and PES2UG21CS569" << std::endl;
     return 0;
 }
